3c9421050c4b27cb508129b9f814f8bcb0144202
Manoti <shrma@softwareag.com>
2023-06-27 22:29:41 +0530

    post api with url validation

--- server.js
+++ server.js
@@ -5,23 +5,28 @@ const axios = require('axios')
 
 app = express();
 
-const isValidUrl = urlString=> {
-    var urlPattern = new RegExp('^(https?:\\/\\/)?'+ // validate protocol
-  '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // validate domain name
-  '((\\d{1,3}\\.){3}\\d{1,3}))'+ // validate OR ip (v4) address
-  '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // validate port and path
-  '(\\?[;&a-z\\d%_.~+=-]*)?'+ // validate query string
-  '(\\#[-a-z\\d_]*)?$','i'); // validate fragment locator
-return !!urlPattern.test(urlString);
+// Body Parser
+app.use(express.json());
+
+function isUrlValid(url) {
+    var res = url.match(/[-a-zA-Z0-9@:%_\+.~#?&\\=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&\\=]*)?/g);
+    if(res == null)
+        return false;
+    else
+        return true;
 }
 
+
 app.post("/api/v1/submiturl", async(req,res,next) => {
-    let url = req.body;
-    const validUrl = isValidUrl(url);
-    console.log(validUrl);
+    let url = req.body.url;
+    const validUrl = isUrlValid(url);
     if(!validUrl){
-        res.status(400).json({success: false});
+        res.status(400).json({success: false, msg: 'Invalid Url, Please try again'});
     }else{
+        axios.get(url)
+        // Show response data
+        .then(res => console.log(res.data))
+        .catch(err => console.log(err))
         res.status(201).json({success: true});
     }
 });
@@ -33,24 +38,24 @@ app.get("/api/v1/submiturl", async(req,res,next) => {
 
 
 // Setting a cron job
-cron.schedule("*/10 * * * * *", function() {
+// cron.schedule("*/10 * * * * *", function() {
 
-	// Data to write on file
-	let data = `${new Date().toUTCString()} : Server is working\n`;
+// 	// Data to write on file
+// 	let data = `${new Date().toUTCString()} : Server is working\n`;
 	
-	// //Appending data to logs.txt file
-	// fs.appendFile("logs.txt", data, function(err) {
+// 	// //Appending data to logs.txt file
+// 	// fs.appendFile("logs.txt", data, function(err) {
 		
-	// 	if (err) throw err;
+// 	// 	if (err) throw err;
 		
-	// 	console.log("Status Logged after 10 seconds!");
-	// });
+// 	// 	console.log("Status Logged after 10 seconds!");
+// 	// });
 
-    // Hitting an API
-    let request = axios.get('https://jsonplaceholder.typicode.com/posts/1')
-        // Show response data
-        .then(res => console.log(res.data))
-        .catch(err => console.log(err))
-});
+//     // Hitting an API
+//     let request = axios.get('https://jsonplaceholder.typicode.com/posts/1')
+//         // Show response data
+//         .then(res => console.log(res.data))
+//         .catch(err => console.log(err))
+// });
 
 app.listen(3000);
